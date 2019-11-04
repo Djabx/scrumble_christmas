333a2b300f23ce69a0f273884a04334f081f6557
Alexandre Badez <alexandre@badez.eu>
2019-11-04 15:05:31 +0100

    Move to python3.

--- README.md
+++ README.md
@@ -1,3 +1,108 @@
+- [What is it ?](#what-is-it)
+- [What do I need ?](#what-do-i-need)
+- [How to use it ?](#how-to-use-it)
+- [What the configuration file should look like ?](#what-the-configuration-file-should-look-like)
+- [Does it always work ?](#does-it-always-work)
+  - [Short Answer](#short-answer)
+  - [Longer Answer](#longer-answer)
+
+
 # What is it ?
 
 A simple script for making gift for christmas.
+
+# What do I need ?
+
+Only [Python3](https://python.org)
+
+# How to use it ?
+
+1. clone this repo (at least copy the `scrumble.py`)
+2. Create a configuration file
+3. launch the command `python3 scrumble.py conf.json`
+
+# What the configuration file should look like ?
+
+```json
+{
+  "couple" : [
+    ["couple_a_person_aa", "couple_a_person_ab"],
+    ["couple_b_person_ba", "couple_b_person_bb"],
+    ["couple_c_person_ca", "couple_c_person_cb"],
+    ["couple_d_person_da", "couple_d_person_db"]
+  ]
+}
+```
+
+# Does it always work ?
+
+## Short Answer
+
+Nope.
+
+The following configuration file have no solution and the current program will loop for ever.
+
+```json
+{
+  "couple" : [
+    ["a", "b"],
+    ["c"]
+  ]
+}
+```
+
+Indeed we have the situation:
+
+```
+a -> c
+c -> b
+b -> a  # <- but it's forbiden to make a gift in the couple
+```
+
+## Longer Answer
+
+This problem could be resume has _one person must have only one gift and make one gift to anybody but his familly_.
+
+Now if we represent it with a graph with those suppositions:
+* vertex represent a person
+* oriented edges represent to who you can make a gift
+
+The following configuration file:
+
+```json
+{
+  "couple" : [
+    ["a", "b"],
+    ["c", "d"]
+  ]
+}
+```
+
+Would make the graph:
+
+```
+a --> c
+a --> d
+b --> c
+b --> d
+c --> a
+c --> b
+d --> a
+d --> d
+```
+
+And it growth in complexity! (In fact it is a [Complete graph](https://en.wikipedia.org/wiki/Complete_graph) minus familly edges).
+
+In graph theory this could be modeled as finding an [Hamiltonian path](https://en.wikipedia.org/wiki/Hamiltonian_path).
+And this is a [NP-Complet](https://en.wikipedia.org/wiki/NP-completeness) problem.
+
+Ok, the general problem is hard, but in our case, we have many edges to walk to (aka we can make a gift to anybody), so most of the time we can find a solution fearly easely.
+
+My implementation here is:
+
+1. init a map of person who make a gift and to who 
+1. take a random person who are not making a gift,
+2. select a random person who do not receive a gift AND is not in the familly,
+3. if you can't find anybody start again (1.)
+4. if nobody left, so everybody have a gift AND everybody is making a gift, you have a solution
+5. merry christmas !
--- scrumble.py
+++ scrumble.py
@@ -1,44 +1,79 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
+#! /bin/env python3
 
-import json
-import random
 import codecs
+import datetime
+import json
+import logging
+import random
+import sys
+import collections
+
+# logging.basicConfig(level=logging.DEBUG)
+
 
 def get_gift(people):
-  return tuple(people)[random.randint(0, len(people)-1)]
+    return tuple(people)[random.randint(0, len(people) - 1)]
+
 
 def scrumble(couples):
-  peoples = set()
-  for c in couples:
-    peoples.update(c)
+    peoples = set()
+    for c in couples:
+        peoples.update(c)
 
-  gifts = {}
-  for i, c in enumerate(couples):
-    couple = set(c)
-    for p in c:
-      have_gift = set(gifts.values())
-      others = peoples - couple - have_gift
-      g = get_gift(others)
-      gifts[p] = g
-  return gifts
+    # Dict with people
+    # X -> Y
+    # X do a gift to Y
+    gifts = collections.OrderedDict()
+    for i, c in enumerate(couples):
+        couple = set(c)
+        for p in c:
+            have_gift = set(gifts.values())
+            others = peoples - couple - have_gift
+            g = get_gift(others)
+            gifts[p] = g
+    return gifts
+
+
+def search_gifts(configuration):
+    while True:
+        try:
+            gifts = scrumble(configuration["couple"])
+            for p, g in gifts.items():
+                print(f"{p:20} -> {g:>20}")
+            return
+        except ValueError as e:
+            pass
+
+
+def load_config(conf_file):
+    with codecs.open(conf_file, "r", "utf8") as fh:
+        configuration = json.load(fh)
+    return configuration
+
+
+def help_message():
+    print(f"Usage of {sys.argv[0]}:")
+    print("{sys.argv[0]} <config_file_to_read>")
+    sys.exit(1)
+
+
+def get_date_to_compute(configuration):
+    # init random to what is given by user
+    default_date = datetime.date.today().isoformat()
+
+    return datetime.date.fromisoformat(configuration.get("date", default_date))
+
+
+def init_random(date_to_compute):
+    # this idea does not work
+    tdelta = date_to_compute - REF_DATE
+    logging.debug("Init seed with: %s", tdelta.total_seconds())
+    random.seed(tdelta.total_seconds())
 
 
 if __name__ == "__main__":
-  with codecs.open('conf.json', 'r', 'utf8') as fh:
-    data = json.load(fh)
-
-  # init random to what is given by user
-  random.seed(data.get("date", None))
-
-  cont = True
-  while cont:
-    try:
-      gifts = scrumble(data["couple"])
-      cont = False
-      for p, g in gifts.iteritems():
-        print u"%(per)15s -> %(gift)s" % {
-          'per' : p, 'gift' : g}
-
-    except ValueError, e:
-      cont = True
+    if len(sys.argv) != 2:
+        help_message()
+    else:
+        configuration = load_config(sys.argv[1])
+        search_gifts(configuration)
